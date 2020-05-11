fd22e0651f1aa032470449fe11488d19395f9016
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-05-11 21:32:36 +0100

    reset puma config

--- config/puma.rb
+++ config/puma.rb
@@ -33,19 +33,19 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # process behavior so workers use less memory.
 #
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-threads threads_count, threads_count
+# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+# threads threads_count, threads_count
 
-preload_app!
+# preload_app!
 
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+# rackup      DefaultRackup
+# port        ENV['PORT']     || 3001
+# environment ENV['RACK_ENV'] || 'development'
 
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
-end
+# on_worker_boot do
+#   ActiveRecord::Base.establish_connection
+# end
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
